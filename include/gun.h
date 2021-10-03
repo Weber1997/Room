37654f993d5d6099327ac23f78371a840f34ff1e
webe <15768757688@outlook.com>
2021-10-03 21:29:20 +0800

    hopefully fixed pathspec error

---
+++ include/gun.h
@@ -0,0 +1,15 @@
+#pragma once 
+#include <string>
+ 
+class Gun
+{
+public:
+    void addBullet(int bullet_num);
+    bool shoot();
+    Gun(std::string  type);
+private:
+    std::string  _type;
+    int _bullet_count;
+};
+
+ 
